207f6b53ce8ecc7db893ee31cc9e4b8ec05d89c1
Khariza Viloria <kharizzakaye@gmail.com>
2022-07-10 19:53:09 +0800

    Round off temperature

--- Weather App/script.js
+++ Weather App/script.js
@@ -27,7 +27,7 @@ let weather = {
         document.querySelector(".city").innerText = name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".temp").innerText = Math.round(temp) + "°C";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
         document.querySelector(".weather").classList.remove("loading");
